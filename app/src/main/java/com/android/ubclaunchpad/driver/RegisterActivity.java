a65cbce30cbc41d4589917349d193b624273cbda
nramley <noor.ramley@gmail.com>
2016-06-01 00:42:07 -0700

    did java

--- app/src/main/java/com/android/ubclaunchpad/driver/RegisterActivity.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/RegisterActivity.java
@@ -2,56 +2,140 @@ package com.android.ubclaunchpad.driver;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.PasswordTransformationMethod;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText etName;
     EditText etEmail;
     EditText etPostalCode;
     EditText password1;
     EditText password2;
     Button bRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        EditText password = (EditText) findViewById(R.id.etPassword);
-        password.setTypeface(Typeface.DEFAULT);
-        password.setTransformationMethod(new PasswordTransformationMethod());
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPostalCode = (EditText) findViewById(R.id.etPostalCode);
+        password1 = (EditText) findViewById(R.id.etPassword);
+        password2 = (EditText) findViewById(R.id.etPasswordConfirm);
+
+        mAuth = FirebaseAuth.getInstance();
+
 
         bRegister = (Button) findViewById(R.id.bSignUp);
 
         bRegister.setOnClickListener(new View.OnClickListener() {
 
-            // currently doesn't link anywhere on success.
+            /**
+             *  Currently links back to SignIn on register success, will link it to new and proper activity when made.
+             *  Not sure how to store user's postal code in the firebase.
+              */
+
             @Override
             public void onClick(View v) {
-                if (v.getId() == R.id.bSignUp) {
-                    password1 = (EditText) findViewById(R.id.etPassword);
-                    password2 = (EditText) findViewById(R.id.etPasswordConfirm);
+                if (!validateBoxes()) {
+                    return;
+                }
 
-                    if (!password1.equals(password2)) {
-                        AlertDialog alertDialog = new AlertDialog.Builder(RegisterActivity.this).create();
-                        alertDialog.setTitle("Uh oh!");
-                        alertDialog.setMessage("Passwords do not match");
-                        alertDialog.setButton("Ok",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        //dismiss the dialog
-                                    }
-                                });
-                        alertDialog.show();
-                    }
+                if (v.getId() == R.id.bSignUp) {
+                    String email = etEmail.getText().toString();
+                    String password = password1.getText().toString();
+                    String name = etName.getText().toString();
+
+                    mAuth.createUserWithEmailAndPassword(email, password);
+                    mAuth.signInWithEmailAndPassword(email, password);
+
+                    // set user's name
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder().setDisplayName(name).build();
+                    user.updateProfile(userProfileChangeRequest);
+
+                    // on to next activity (for now just leads back to SignIn)
+                    Intent nextIntent = new Intent(RegisterActivity.this, SignInActivity.class);
+                    RegisterActivity.this.startActivity(nextIntent);
+
                 }
             }
         });
+    }
+
+
+
+    private boolean validateBoxes() {
+        boolean valid = true;
+
+        String name = etName.getText().toString();
+        if (TextUtils.isEmpty(name)) {
+            etName.setError("Required.");
+            valid = false;
+        } else {
+            etName.setError(null);
+        }
+
+        String email = etEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            etEmail.setError("Required.");
+            valid = false;
+        } else {
+            etEmail.setError(null);
+        }
+
+        String postalCode = etPostalCode.getText().toString();
+        if (TextUtils.isEmpty(postalCode)) {
+            etPostalCode.setError("Required.");
+            valid = false;
+        } else {
+            etPostalCode.setError(null);
+        }
+
+
+        String passwordFirst = password1.getText().toString();
+        if (TextUtils.isEmpty(passwordFirst)) {
+            password1.setError("Required.");
+            valid = false;
+        } else {
+            password1.setError(null);
+        }
+
+        String passwordSecond = password2.getText().toString();
+        if (TextUtils.isEmpty(passwordSecond)) {
+            password2.setError("Required.");
+            valid = false;
+        } else {
+            password2.setError(null);
+        }
+
+
+        if (!passwordFirst.equals(passwordSecond)) {
+            AlertDialog alertDialog = new AlertDialog.Builder(RegisterActivity.this).create();
+            alertDialog.setTitle("Uh oh!");
+            alertDialog.setMessage("Passwords do not match");
+            alertDialog.setButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //dismiss the dialog
+                        }
+                    });
+            alertDialog.show();
+            valid = false;
+        }
+
+        return valid;
     }
 }
--- app/src/main/java/com/android/ubclaunchpad/driver/SignInActivity.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/SignInActivity.java
@@ -1,74 +1,138 @@
 package com.android.ubclaunchpad.driver;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.firebase.client.AuthData;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private TextView signInLink;
+    private ProgressDialog pd;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        Firebase.setAndroidContext(this);
 
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final TextView signInLink = (TextView) findViewById(R.id.bSignIn);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        signInLink = (TextView) findViewById(R.id.bSignIn);
 
-        final Firebase firebaseApp = new Firebase("driver-7987b.firebaseio.com");
+        mAuth = FirebaseAuth.getInstance();
 
-        signInLink.setOnClickListener(new View.OnClickListener() {
+        // Handles authentication changes on UI end
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                } else {
+                    // User is signed out
 
+                }
+            }
+        };
+
+
+        // On-clicks
+        findViewById(R.id.bSignIn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-
-                firebaseApp.authWithPassword(email, password, new Firebase.AuthResultHandler() {
-                    @Override
-                    public void onAuthenticated(AuthData authData) {
-                        
-                        // will change this to an intent leading to next page
-                        AlertDialog alertDialog = new AlertDialog.Builder(SignInActivity.this).create();
-                        alertDialog.setTitle("Success!");
-                        alertDialog.setMessage("You're logged in");
-                        alertDialog.setButton("Ok",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                       //goes away
-                                    }
-                                });
-                        alertDialog.show();
-                    }
-
-                    @Override
-                    public void onAuthenticationError(FirebaseError firebaseError) {
-                        AlertDialog alertDialog = new AlertDialog.Builder(SignInActivity.this).create();
-                        alertDialog.setTitle("Wrong credentials!");
-                        alertDialog.setMessage("You're not logged in");
-                        alertDialog.setButton("Ok",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        //goes away
-                                    }
-                                });
-                        alertDialog.show();
-                    }
-                });
+                signIn(etUsername.getText().toString(), etPassword.getText().toString());
 
             }
         });
 
+        findViewById(R.id.tvSignUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(SignInActivity.this, RegisterActivity.class);
+                SignInActivity.this.startActivity(registerIntent);
+            }
+        });
 
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+
+    // Signs in and handles UI notifications
+    private void signIn(String email, String password) {
+        if (!validateForm()) {
+            return;
+        }
+
+        // Sign in with email/password
+        mAuth.signInWithEmailAndPassword(email, password);
+
+        // On Success, leads to next activity
+        if (mAuth.getCurrentUser() != null) {
+            Toast.makeText(getApplicationContext(), "Signed in", Toast.LENGTH_SHORT).show();
+            /**
+             * Leads to next activity, when we make it (Driver/Passenger select?)
+             */
+            // Intent nextIntent = new Intent(SignInActivity.this, DriverPassengerActivity.class);
+            // SignInActivity.this.startActivity(nextIntent);
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Invalid email/password", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+
+        String email = etUsername.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            etUsername.setError("Required.");
+            valid = false;
+        } else {
+            etUsername.setError(null);
+        }
+
+        String password = etPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            etPassword.setError("Required.");
+            valid = false;
+        } else {
+            etPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+
 }
