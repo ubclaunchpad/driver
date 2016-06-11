248352ad399a568a69eed166920b7d46ad3f5d48
Kelvin Chan <chan.kelv@gmail.com>
2016-06-11 15:26:07 -0700

    added method for grabbing info and parsing to value

--- app/src/main/java/com/android/ubclaunchpad/driver/util/HardwareUtils.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/util/HardwareUtils.java
@@ -26,7 +26,11 @@ public class HardwareUtils {
             Log.e("getCpuInfoMap", Log.getStackTraceString(e));}
         return map;
     }
-
+// TODO remove this method - it is only used as an example for now
+//    /**
+//     * Determines the BogoMIPS based on proc/cpuinfo from the phones stats
+//     * @return The numeric BogoMIPS value. Zero if no MIPS found
+//     */
 //    public static double getBogoMIPS(){
 //        Map<String, String> cpuInfo = getCpuInfoMap();
 //        if(cpuInfo.size() >= 0 && cpuInfo.containsKey("BogoMIPS")){
@@ -34,13 +38,49 @@ public class HardwareUtils {
 //                double mips = Double.parseDouble(cpuInfo.get("BogoMIPS"));
 //                return mips;
 //            }
-//            catch (NullPointerException){
-//
+//            catch (NullPointerException e){
+//                Log.e("Hardware Util", "Null error  " + e.getMessage());
 //            }
-//            catch (NumberFormatException){
-//
+//            catch (NumberFormatException e){
+//                Log.e("Hardware Util", "Number format error " + e.getMessage());
 //            }
 //        }
-//        return
+//        return 0; //default return 0
 //    }
+
+    /**
+     * Determines the numeric value based on proc/cpuinfo from the phones stats
+     * This method only works for fields that return a number value such as
+     * "BogoMIPS" and "Processor"
+     * @return The numeric BogoMIPS value. Zero if no MIPS found
+     */
+    public static double getPhoneValue(Map<String, String> cpuInfo, String infoKey){
+        if(cpuInfo.size() >= 0 && cpuInfo.containsKey(infoKey)){
+            try {
+                double infoValue = Double.parseDouble(cpuInfo.get(infoKey));
+                return infoValue;
+            }
+            catch (NullPointerException e){
+                Log.e("Hardware Util", "Null " + e.getMessage());
+            }
+            catch (NumberFormatException e){
+                Log.e("Hardware Util", "Number format " + e.getMessage());
+            }
+            return 0; //default return 0
+        }
+        return 0;
+    }
+
+    /**
+     * Compares the stats of two devices and returns true if "myPhone" is faster than the "otherPhone"
+     * "Faster" in this context is based on the BogoMIPS and the higher is considered faster
+     * By arbitrary reasons, if BogoMIPS == 0, the # of processor * 5.5 will be the BogoMIPS value used
+     * @param myPhone cpuInfo of this device
+     * @param otherPhone cpuInfo of other device
+     * @return true if myPhone BogoMIPS > otherPhone BogoMIPS
+     */
+    public static boolean isPhoneFaster(Map<String, String> myPhone, Map<String, String> otherPhone){
+        //TODO implement
+        return false;
+    }
 }
