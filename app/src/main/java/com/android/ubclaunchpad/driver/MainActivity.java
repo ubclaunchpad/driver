bc57691ee05c8eb3508203c95b3ef48053e9e38d
MickAvery <mav.cuyugan@gmail.com>
2016-06-25 14:30:47 -0700

    commented out login check in DispatchActivity for now, added buttons in MainActivity

--- app/src/main/java/com/android/ubclaunchpad/driver/MainActivity.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/MainActivity.java
@@ -2,12 +2,31 @@ package com.android.ubclaunchpad.driver;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button iAmADriverButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iAmADriverButton = (Button) findViewById(R.id.i_am_a_driver_button);
+        iAmADriverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(v.getContext(), "I am a driver lol", Toast.LENGTH_SHORT).show();
+                EditText howManyPassengers = new EditText(v.getContext());
+                howManyPassengers.setLayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT));
+                ((EditText) findViewById(R.id.how_many_passengers)).setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
