259e2d48e35bd3099b9f571ba8e4d8df393312c1
MickAvery <mav.cuyugan@gmail.com>
2016-07-03 21:42:22 -0700

    cleaned up code, added margin space on top of dialog header

--- app/src/main/java/com/android/ubclaunchpad/driver/UI/NumPassengersFragment.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/UI/NumPassengersFragment.java
@@ -7,11 +7,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
@@ -50,14 +46,11 @@ public class NumPassengersFragment extends DialogFragment {
         builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Log.d("PRESS CANCEL", "cancel button pressed");
             }
         });
         builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Log.d("PRESS OK", "ok button pressed");
-
                 int numPassengers = numPassengerPick.getValue();
 
                 // debug toad
