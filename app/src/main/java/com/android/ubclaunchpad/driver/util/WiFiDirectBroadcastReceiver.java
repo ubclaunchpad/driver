904590f7a2da05285ca3cd8aa0d0a24ac21bf1ef
Kelvin Chan <chan.kelv@gmail.com>
2016-09-10 14:07:26 -0700

    no message

---
+++ app/src/main/java/com/android/ubclaunchpad/driver/util/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,71 @@
+package com.android.ubclaunchpad.driver.util;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
+
+/**
+ * Created by kelvinchan on 2016-09-03.
+ */
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver{
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private Activity mActivity;
+
+    WifiP2pManager.PeerListListener myPeerListListener;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                       Activity activity) {
+        super();
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mActivity = activity;
+
+        myPeerListListener = new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList peers) {
+                Log.d("WIFI", peers.toString());
+            }
+        };
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                // Wifi P2P is enabled
+            } else {
+                // Wi-Fi P2P is not enabled
+            }
+        }
+
+        if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            // request available peers from the wifi p2p manager. This is an
+            // asynchronous call and the calling activity is notified with a
+            // callback on PeerListListener.onPeersAvailable()
+            if (mManager != null) {
+                mManager.requestPeers(mChannel, myPeerListListener);
+            }
+        }
+    }
+
+
+}
