fe7f18aa82e4e8899ed313ca513e20de77e6c27b
MickAvery <mav.cuyugan@gmail.com>
2016-09-24 09:54:15 -0700

    Connects to all detected devices in WifiBroadcastReceiver

--- app/src/main/java/com/android/ubclaunchpad/driver/util/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/util/WiFiDirectBroadcastReceiver.java
@@ -4,9 +4,15 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kelvinchan on 2016-09-03.
@@ -15,6 +21,8 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver{
     private WifiP2pManager mManager;
     private WifiP2pManager.Channel mChannel;
     private Activity mActivity;
+    private WifiP2pDeviceList mPeers;
+    private List<WifiP2pDevice> mDeviceList = new ArrayList<WifiP2pDevice>();
 
     WifiP2pManager.PeerListListener myPeerListListener;
 
@@ -28,7 +36,26 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver{
         myPeerListListener = new WifiP2pManager.PeerListListener() {
             @Override
             public void onPeersAvailable(WifiP2pDeviceList peers) {
-                Log.d("WIFI", peers.toString());
+                mDeviceList.clear();
+                mDeviceList.addAll(peers.getDeviceList());
+
+                for(WifiP2pDevice device : mDeviceList) {
+                    WifiP2pConfig config = new WifiP2pConfig();
+                    config.deviceAddress = device.deviceAddress;
+                    config.wps.setup = WpsInfo.PBC;
+
+                    mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+                        @Override
+                        public void onSuccess() {
+                            // good, we're connected
+                        }
+
+                        @Override
+                        public void onFailure(int reason) {
+                            Log.d("Connect Failure", "Failed to connect");
+                        }
+                    });
+                }
             }
         };
     }
@@ -40,7 +67,12 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver{
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
             // Check to see if Wi-Fi is enabled and notify appropriate activity
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-            // Call WifiP2pManager.requestPeers() to get a list of current peers
+            // request available peers from the wifi p2p manager. This is an
+            // asynchronous call and the calling activity is notified with a
+            // callback on PeerListListener.onPeersAvailable()
+            if (mManager != null) {
+                mManager.requestPeers(mChannel, myPeerListListener);
+            }
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
             // Respond to new connection or disconnections
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
@@ -53,16 +85,6 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver{
                 // Wifi P2P is enabled
             } else {
                 // Wi-Fi P2P is not enabled
-            }
-        }
-
-        if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-
-            // request available peers from the wifi p2p manager. This is an
-            // asynchronous call and the calling activity is notified with a
-            // callback on PeerListListener.onPeersAvailable()
-            if (mManager != null) {
-                mManager.requestPeers(mChannel, myPeerListListener);
             }
         }
     }
