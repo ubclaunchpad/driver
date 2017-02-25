9621190d07f627b1f2552ad203f16c7f63753dff
Sherry Yuan <frostyyshadows@gmail.com>
2017-02-25 13:45:01 -0800

    removed session name

--- app/src/main/java/com/android/ubclaunchpad/driver/models/SessionModel.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/models/SessionModel.java
@@ -79,7 +79,6 @@ public class SessionModel {
     public static SessionModel createNewSession(String name, LatLng latLng) {
 
         SessionModel sessionModel = new SessionModel(latLng);
-        sessionModel.setName(name);
 
         try {
             User user = UserManager.getInstance().getUser();
