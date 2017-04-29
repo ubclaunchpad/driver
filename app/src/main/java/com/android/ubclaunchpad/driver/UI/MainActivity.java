07e85a6f4d17d0d00b943449d1c0c54ac7c6f01a
Luxi Zhao <lucyzhao.zlx@gmail.com>
2017-04-29 15:33:23 -0700

    added action bar to MainActivity

--- app/src/main/java/com/android/ubclaunchpad/driver/UI/MainActivity.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/UI/MainActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -33,16 +36,25 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.i_am_a_passenger_button) Button mPassengerButton;
     @BindView(R.id.i_am_a_driver_button) Button mDriverButton;
     @BindView(R.id.button3) Button mSessionButton;
-    @BindView(R.id.sign_out_button) Button mSignOutButton;
+   // @BindView(R.id.sign_out_button) Button mSignOutButton;
 
     private static Context context;
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
 
         context = getApplicationContext();
 
@@ -105,18 +117,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mSignOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DispatchActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                Log.d(TAG, "onClick: Signing Out!");
-                FirebaseAuth.getInstance().signOut();
-                startActivity(intent);
-                finish();
-            }
-        });
-    
+//        mSignOutButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, DispatchActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                Log.d(TAG, "onClick: Signing Out!");
+//                FirebaseAuth.getInstance().signOut();
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
 
 /**
  * TODO this button is for demo purposes. When the UI team comes and changes the UI, please REMOVE
@@ -137,5 +148,39 @@ public class MainActivity extends AppCompatActivity {
 
     public static Context getContext(){
         return MainActivity.context;
+    }
+
+    public void signOut(MenuItem menuItem){
+        Intent intent = new Intent(MainActivity.this, DispatchActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Log.d(TAG, "Signing Out");
+        FirebaseAuth.getInstance().signOut();
+        startActivity(intent);
+        finish();
+    }
+
+    public void editProfile(MenuItem menuItem){
+        Log.d(TAG, "editing profile");
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sign_out:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_edit_profile:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
