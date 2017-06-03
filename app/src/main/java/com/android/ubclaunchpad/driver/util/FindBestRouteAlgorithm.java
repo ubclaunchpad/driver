368ba0cd279cc190b6bedd8b7ccf9115492341e6
grigorii <prb578@icloud.com>
2017-06-03 13:48:19 -0700

    Moved functions creating test cases to test classes

--- app/src/main/java/com/android/ubclaunchpad/driver/util/FindBestRouteAlgorithm.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/util/FindBestRouteAlgorithm.java
@@ -153,7 +153,6 @@ public class FindBestRouteAlgorithm {
                 }
             }
             if (optimalDriver != null) {
-
                 optimalDriver.addPassenger(currentPassenger);
             }
         }
