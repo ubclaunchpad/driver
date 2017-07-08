64a2c6416bd2141aa43330744a8e1347571bd61a
Sherry Yuan <frostyyshadows@gmail.com>
2017-07-08 12:19:04 -0700

    some work done

--- app/src/main/java/com/android/ubclaunchpad/driver/network/GoogleDirectionsApi.java
+++ app/src/main/java/com/android/ubclaunchpad/driver/network/GoogleDirectionsApi.java
@@ -10,44 +10,20 @@ import okhttp3.Response;
 /**
  * Created by sherryuan on 2017-07-03.
  */
-
 public class GoogleDirectionsApi {
 
-    // requests take the form of https://maps.googleapis.com/maps/api/directions/outputFormat?parameters
-    // outputFormat can be json or xml, we'll use json
-    private static String DIRECTIONS_BASE_URL = "https://maps.googleapis.com/maps/api/directions/json?";
+    // base URL for launching an Intent to the Google Maps app
+    // https://developers.google.com/maps/documentation/urls/guide
+    private static String DIRECTIONS_BASE_URL = "https://www.google.com/maps/dir/?api=1&";
     private static String ORIGIN = "origin=";
     private static String DESTINATION = "destination=";
     private static String WAYPOINTS = "waypoints=";
-    private static String API_KEY = "key=AIzaSyBviR5jNBd9ktpZMb25bmb8ZlK1MOJMEyY";
-    private static String AMBERSAND = "&";
-
-    private NetworkUtils networkUtils = new NetworkUtils();
-
-    private Callback directionsCallback = new Callback() {
-
-        @Override
-        public void onFailure(Call call, IOException e) {
-
-        }
-
-        @Override
-        public void onResponse(Call call, Response response) throws IOException {
-
-        }
-    };
-
-    public void getDirections(String originLatLng, String destinationLatLng, List<String> waypoints) {
-        networkUtils.GetRequestAsync(
-                encodeURL(originLatLng, destinationLatLng, waypoints),
-                directionsCallback
-        );
-    }
+    private static String AMPERSAND = "&";
 
     public String encodeURL(String originLatLng, String destinationLatLng, List<String> waypoints) {
         String encodedUrl = DIRECTIONS_BASE_URL +
                 ORIGIN + originLatLng +
-                AMBERSAND + DESTINATION + destinationLatLng;
+                AMPERSAND + DESTINATION + destinationLatLng;
 
         if (!waypoints.isEmpty()) {
             String wayPointsString = "";
@@ -56,8 +32,8 @@ public class GoogleDirectionsApi {
             }
             // remove last pipe character
             wayPointsString = wayPointsString.substring(0, wayPointsString.length() - 1);
-            encodedUrl = encodedUrl.concat(AMBERSAND + WAYPOINTS + wayPointsString);
+            encodedUrl = encodedUrl.concat(AMPERSAND + WAYPOINTS + wayPointsString);
         }
-        return encodedUrl + AMBERSAND + API_KEY;
+        return encodedUrl;
     }
 }
--- app/src/test/java/com/android/ubclaunchpad/driver/network/GoogleDirectionsApiTest.java
+++ app/src/test/java/com/android/ubclaunchpad/driver/network/GoogleDirectionsApiTest.java
@@ -19,7 +19,7 @@ public class GoogleDirectionsApiTest {
         // test case where waypoints are empty
         String urlWithOutWaypoints = googleDirectionsApi.encodeURL("41.43206,-81.38992", "41.412,-81.245", new ArrayList<String>());
         assertEquals(urlWithOutWaypoints,
-                "https://maps.googleapis.com/maps/api/directions/json?origin=41.43206,-81.38992&destination=41.412,-81.245&key=AIzaSyBviR5jNBd9ktpZMb25bmb8ZlK1MOJMEyY");
+                "https://www.google.com/maps/dir/?api=1&origin=41.43206,-81.38992&destination=41.412,-81.245");
 
         // test case with multiple waypoints
         List<String> wayPoints = new ArrayList<>();
@@ -27,7 +27,7 @@ public class GoogleDirectionsApiTest {
         wayPoints.add("-34.92788,138.60008");
         String urlWithWaypoints = googleDirectionsApi.encodeURL("-33.8688,151.2093", "-31.9505,115.8605", wayPoints);
         assertEquals(urlWithWaypoints,
-                "https://maps.googleapis.com/maps/api/directions/json?origin=-33.8688,151.2093&destination=-31.9505,115.8605&waypoints=-37.81223,144.96254|-34.92788,138.60008&key=AIzaSyBviR5jNBd9ktpZMb25bmb8ZlK1MOJMEyY"
+                "https://www.google.com/maps/dir/?api=1&origin=-33.8688,151.2093&destination=-31.9505,115.8605&waypoints=-37.81223,144.96254|-34.92788,138.60008"
         );
     }
 }
